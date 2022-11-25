6302b5f15ce1a46ff153ab8d9cdd4b4091969b18
hanaelle <hananeelkarouni1@gmail.com>
2022-11-25 10:12:04 +0100

    projet

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <h1 className='titre bg-red-300'>Weather App</h1>
+  )
+}
+
+export default App
