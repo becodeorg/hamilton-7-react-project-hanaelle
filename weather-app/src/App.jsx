84df98c3a8ddbc291076c23e154fb3d574354f3c
hanaelle <hananeelkarouni1@gmail.com>
2022-11-28 17:19:21 +0100

    loading and error

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -9,36 +9,7 @@ function App() {
     const[geoLoc, setGeoLoc] = useState({latitude:  0, longitude: 0});     //Une valeur est attribuée à l'erreur, mais elle n'est jamais utilisée.
     const[weatherUnits, setWeatherUnits] = useState({});
     const [weatherData, setWeatherData] = useState([]);
-    useEffect (() => {
-        setIsLoading(true);
-
-        if(!navigator.geolocation) {                                         //pour veridier si le navigateur est compatible (! navigateur. geo ...) veut dire s il n exite pas ce sera pas possible del utiliser 
-            window.alert(
-            "votre naguation ne permet pas la géolocalisation pour utiliser cette application"//infome l utilisateur qu on peut pas lui donner cet indo
-            );
-        }
-    getGeoLocalisation();                                                 // ici on va recupere les donnes gps
-
-    fetchWeather(
-        ´https://api.open-meteo.com/v1/forecast?latitude=${geoLoc.latitude}&longitude=${geoLoc.longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon´
-);
-
-    }, []);
-
-    const getGeoLocalisation = () => {                                       //chercher les donnees gps
-        navigator.geolocation.getCurrentPosition(                            //cette fonction reponds en cas ed succes 
-            (position) => {
-                setGeoLoc({                                                 //si on reussi a avoir l info on aura acces a la lattitude et la longitude.ici (position.coords.latitude)le coords se trouve sur le site des coordonne 
-                    latitude: position.coords.latitude, 
-                    longitude: position.coords.longitude,
-                }); 
-            },
-            () => {
-                setError(true);                                                 // en cas d erreur faire ce call back 
-            }
-        );  
-    };
-
+    
     const fetchWeather = useCallback(async (url) => {
         setError(false);
 
@@ -66,9 +37,56 @@ function App() {
             }    
         }catch (error) {}
 },[]);
+    useEffect (() => {
+        setIsLoading(true);
+
+        if(!navigator.geolocation) {                                         //pour veridier si le navigateur est compatible (! navigateur. geo ...) veut dire s il n exite pas ce sera pas possible del utiliser 
+            window.alert(
+            "votre naguation ne permet pas la géolocalisation pour utiliser cette application"//infome l utilisateur qu on peut pas lui donner cet indo
+            );
+        }
+    getGeoLocalisation();                                                 // ici on va recupere les donnes gps
+
+    fetchWeather(
+        `https://api.open-meteo.com/v1/forecast?latitude=${geoLoc.latitude}&longitude=${geoLoc.longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon`
+    ).then(() => setIsLoading(false));
+
+    }, [fetchWeather, geoLoc.latitude, geoLoc.longitude]);
+
+    const getGeoLocalisation = () => {                                       //chercher les donnees gps
+        navigator.geolocation.getCurrentPosition(                            //cette fonction reponds en cas ed succes 
+            (position) => {
+                setGeoLoc({                                                 //si on reussi a avoir l info on aura acces a la lattitude et la longitude.ici (position.coords.latitude)le coords se trouve sur le site des coordonne 
+                    latitude: position.coords.latitude, 
+                    longitude: position.coords.longitude,
+                }); 
+            },
+            () => {
+                setError(true);                                                 // en cas d erreur faire ce call back 
+            }
+        );  
+    };
+//si c est en chargement
+    if (isLoading) {
+        return(
+            <div>
+                <p>Chargement ...</p>
+            </div>
+        )
+        
+    }
+//si il y a une erreur de chargement 
+    if (error) {
+        return(
+            <div>
+                <p>Une erreur est survenue lors de la récuperation des previsions météo ...</p>
+            </div>
+        )
+        
+    }
 
 
-  return <div className='App'>METEO</div>;
+  return <div className=""></div>;
   
 }
 export default App
