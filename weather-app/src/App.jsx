ddea0c90ee5576bbcb439d356f19b3f492914418
hanaelle <hananeelkarouni1@gmail.com>
2022-11-28 16:14:00 +0100

    format weather data daily

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,83 +1,74 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
+//import reactLogo from './assets/react.svg'
 import './App.css'
+import { formatWeatherDataDaily } from './utils/formatWeatherDataDaily'
 
 function App() {
-    const[isloading, setIsLoading] = useState(false); //  se voit attribuer une valeur mais n'est jamais utilisé.
-    const[error, setError] = useState(false);  //Une valeur est attribuée à l'erreur, mais elle n'est jamais utilisée.
-    const[geoLoc, setGeoLoc] = useState({latitude:  0, longitude: 0});  //Une valeur est attribuée à l'erreur, mais elle n'est jamais utilisée.
-    const[weatherUnits, setWeatherUnits] = useState({})
+    const[isloading, setIsLoading] = useState(false);                       //  se voit attribuer une valeur mais n'est jamais utilisé.
+    const[error, setError] = useState(false);                              //Une valeur est attribuée à l'erreur, mais elle n'est jamais utilisée.
+    const[geoLoc, setGeoLoc] = useState({latitude:  0, longitude: 0});     //Une valeur est attribuée à l'erreur, mais elle n'est jamais utilisée.
+    const[weatherUnits, setWeatherUnits] = useState({});
+    const [weatherData, setWeatherData] = useState([]);
     useEffect (() => {
         setIsLoading(true);
 
-    if(!navigator.geolocation) {//pour veridier si le navigateur est compatible (! navigateur. geo ...) veut dire s il n exite pas ce sera pas possible del utiliser 
-        window.alert(
+        if(!navigator.geolocation) {                                         //pour veridier si le navigateur est compatible (! navigateur. geo ...) veut dire s il n exite pas ce sera pas possible del utiliser 
+            window.alert(
             "votre naguation ne permet pas la géolocalisation pour utiliser cette application"//infome l utilisateur qu on peut pas lui donner cet indo
-        );
-    }
-    getGeoLocalisation(); // ici on va recupere les donnes gps
+            );
+        }
+    getGeoLocalisation();                                                 // ici on va recupere les donnes gps
 
-    fetchWeather("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon"
+    fetchWeather(
+        ´https://api.open-meteo.com/v1/forecast?latitude=${geoLoc.latitude}&longitude=${geoLoc.longitude}&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,windspeed_10m_max&timezone=Europe%2FLondon´
 );
 
     }, []);
 
-    const getGeoLocalisation = () => {
-        navigator.geolocation.getCurrentPosition(  //cette fonction reponds en cas ed succes 
+    const getGeoLocalisation = () => {                                       //chercher les donnees gps
+        navigator.geolocation.getCurrentPosition(                            //cette fonction reponds en cas ed succes 
             (position) => {
-                setGeoLoc({       //si on reussi a avoir l info on aura acces a la lattitude et la longitude.ici (position.coords.latitude)le coords se trouve sur le site des coordonne 
+                setGeoLoc({                                                 //si on reussi a avoir l info on aura acces a la lattitude et la longitude.ici (position.coords.latitude)le coords se trouve sur le site des coordonne 
                     latitude: position.coords.latitude, 
                     longitude: position.coords.longitude,
                 }); 
             },
             () => {
-                setError(true); // en cas d erreur faire ce call back 
+                setError(true);                                                 // en cas d erreur faire ce call back 
             }
         );  
     };
-const fetchWeather = useCallback(async (url) => {
-    setError(false);
 
-    try {
-        const res = await fetch (url); //res => response => promesse
-        const data = res.json();
+    const fetchWeather = useCallback(async (url) => {
+        setError(false);
 
-        if(object.keys(data).length == 0){
-            setError(true);
-        } else{
-            // recupere les donnees jour par jour mais on va les formater
-            // recupere les unites (leve du soleil couche du soeil etc..on recupere ses donnees sur le site .)
-            setWeatherUnits({
-                rain: data.daily_units.precipitation_sun,
-                temperature: data.daily_units.temperature_2m_max,
-                wind: data.daily_units.windspeed_10m_max,
-            })
-        }    
-    
-    }catch (error) {}
+        try {
+            const res = await fetch (url);                                  //res => response => promesse
+            const data = res.json();
+
+            
+            
+            console.log(data);
+            if (Object.keys(data).length === 0) {
+                setError(true);
+            } else {                        // recupere les donnees jour par jour mais on va les formater
+                const formattedDailyData = formatWeatherDataDaily(data.daily);    
+                setWeatherData(formattedDailyData);
+                    // recupere les unites (leve du soleil couche du soeil etc..on recupere ses donnees sur le site .)
+                
+                
+                
+                    setWeatherUnits({
+                    rain: data.daily_units.precipitation_sum,
+                    temperature: data.daily_units.temperature_2m_max,
+                    wind: data.daily_units.windspeed_10m_max,
+                });
+            }    
+        }catch (error) {}
 },[]);
 
 
-  return (
-    <main className='container  space-y-4'>
-        <div className='title bg-teal-400'>
-          <h1 className='title-h1 pt-0 text-5xl text-white text-center'>Weather App</h1>
-        </div>
-        <p className='city text-xl bg-red-600 space-y-4'>
-            Enter a city
-          </p>
-
-          
-        
-        
-        
-        
-            <input className='input-text flex flex-col space-y-4 bg-slate-400' type="text" />
-
-<button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-6 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-  Enter
-</button>
-    </main>
-  )
+  return <div className='App'>METEO</div>;
+  
 }
 export default App
--- weather-app/src/utils/formatWeatherDataDaily.jsx
+++ weather-app/src/utils/formatWeatherDataDaily.jsx
@@ -1,4 +1,39 @@
-// ici j exporte le tableau qui correspont au jour de la semaien avec chaque unite (temp, degres, vent etc...)
+export const formatWeatherDataDaily = (data) => {   // ici j exporte le tableau qui correspont au jour de la semaien avec chaque unite (temp, degres, vent etc...)
+    const dataDaily = [];  //tableau vide qui reprend les jours de la semaine
+
+    const dataEntries = Object.keys(data);                              //donnes qui represente jour par jour 
+
+    dataEntries.forEach((key, keyIndex) => {
+        for (let i = 0; i < data[key].length; i++) {                    //dans cette boucle FOR une passe sur le tableau des jours pour definir chaque valeur
+            if(keyIndex === 0) {                                         // SI key Index est = 0 c est a dire commencer le 1 err tour du tableau 
+                dataDaily.push({});                                      //on rajouter autant de donnee dans notre objet data
+        }
+            const dayValue = data[key][i];                              //on recupere la valeur du jour individuellemtn jour par jour  dans le champs en question 
+            dataDaily[i][key] = dayValue;                                // placer la donnee du jour de l objet qui correspont au jour en question 
+        }
+    });  
+    dataDaily.forEach((data) => {                                            // jour en Francais
+        const date = newDate(date.time);
+        const dayIndex = date.getDay();                                   // renvois un index avc 0 pour dimache 1 lundi etc...6 pour samedi 
+        data.day = frenchDays[dayIndex];
+    });  
+    return dataDaily;                                      
+};
+
+    const frenchDays = [
+        "Dimanche", 
+        "Lundi",
+        "Mardi", 
+        "Mercredi",
+        "Jeudi", 
+        "Vendredi",
+        "Samedi",
+    ];
+
+
+
+
+
 //         ici ca correspondt au taleau des jours de la semaine avec toutes les donnes du lundi eu dimanche
 //     [ 
 //         {
@@ -15,10 +50,3 @@
 //         },
 //     ]
 // 
-
-export const formatWeatherDataDaily = (data) => {
-    const dataDaily = []  //tableau vide qui reprend les jours de la semaine
-
-    const dataEntries = Object.keys(data)   //donnes qui represente jour par jour 
-
-}
